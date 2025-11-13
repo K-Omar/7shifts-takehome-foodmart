37526393c37ecc2650c7e3beffd206af44237794
Omar Chaudhry <omarkhayyam.ch@gmail.com>
2025-11-12 20:16:37 -0500

    7shifts-TakeHome: Initial project setup

---
+++ FoodMartOriginal/FoodMartOriginalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMartOriginalApp.swift
+//  FoodMartOriginal
+//
+//  Created by Omar Chaudhry on 2025-11-12.
+//
+
+import SwiftUI
+
+@main
+struct FoodMartOriginalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
