7c32ef83f27cb4ab816afe91469e8c6026f4b4bc
Omar Chaudhry <omarkhayyam.ch@gmail.com>
2025-11-16 18:47:14 -0500

    7shifts-TakeHome: Add unit test target

---
+++ FoodMartOriginalTests/FoodMartOriginalTests.swift
@@ -0,0 +1,15 @@
+//
+//  FoodMartOriginalTests.swift
+//  FoodMartOriginalTests
+//
+//  Created by Omar Chaudhry on 2025-11-16.
+//
+
+@testable import FoodMartOriginal
+import Foundation
+import Testing
+
+@MainActor
+struct FoodMartOriginalTests {
+
+}
